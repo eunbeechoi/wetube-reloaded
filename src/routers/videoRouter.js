326a131d1878f81a1a47d0ff2976aa076a717ee5
eunbeechoi <noninvertingamp@gmail.com>
2023-08-30 16:40:45 +0900

    #6.4

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
-import {see, edit, upload, deleteVideo} from "../controllers/videoController";
+import {watch, getEdit, postEdit} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see); //digit(숫자)만 받아옴 
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+
+videoRouter.get("/:id(\\d+)", watch); //digit(숫자)만 받아옴 
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
