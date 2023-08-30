f113cd4041587350e7995aae5f4b1b101be3c34e
eunbeechoi <noninvertingamp@gmail.com>
2023-08-30 11:13:09 +0900

    #5.8 iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,7 @@
-const fakeUser = {
-    username: "seya",
-    loggedIn: true,
+export const trending = (req, res) => {
+    const videos = [];
+   return res.render("home", {pageTitle: "Home", videos});
 };
-
-
-
-
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser: fakeUser});
 export const see = (req, res) => res.render("watch");
 
 export const edit = (req, res) => res.render("edit");
