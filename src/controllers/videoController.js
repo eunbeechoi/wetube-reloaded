f521828b7fa5d09a690013e98ba4d7f45aaf191e
eunbeechoi <noninvertingamp@gmail.com>
2023-09-05 22:12:10 +0900

    #6.24 Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -38,18 +38,15 @@ export const getEdit = async(req, res) => {
 export const postEdit = async(req, res) => {
     const { id } = req.params;
     const { title, description, hashtags} = req.body;
-    const video = await Video.exist({_id: id});
+    const video = await Video.exists({_id: id});
     if(!video) {
         return res.render("404", {pageTitle: "Video not found."});
     }
     await Video.findByIdAndUpdate(id, { 
-        title, description,
-        hashtags: hashtags
-        .split(",")                // array로 변환
-        .map((word) => (word.startsWith("#") ? word :  `#${word}`)),
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags),
     });
-
-    await video.save();
     return res.redirect(`/videos/${id}`);
     
 };
@@ -65,7 +62,7 @@ export const postUpload = async (req, res) => {
             title,
             description,
             createdAt: Date.now(),
-            hashtags, 
+            hashtags: Video.formatHashtags(hashtags),
            });
         return res.redirect("/");
     } catch(error){
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+export const formatHashtags = (hashtags) => 
+    hashtags.split(",").map((word) => (word.startsWith("#") ? word :  `#${word}`))
+
 //model을 생성하기 전에 model의 형태 정의 (schema)
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true, trim: true, maxLength: 80},
@@ -12,11 +15,11 @@ const videoSchema = new mongoose.Schema({
     },
 });
 
-videoSchema.pre("save", async function() {
-    this.hashtags = this.hashtags[0]
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
-});
+videoSchema.static('formatHashtags', function(hashtags) {
+    return hashtags.split(",").map((word) => (word.startsWith("#") ? word :  `#${word}`))
+
+})
+
 
 //모델을 만들어 준다. 
 const Video = mongoose.model("Video", videoSchema);  // mongoose.model("모델 이름", Schema)
