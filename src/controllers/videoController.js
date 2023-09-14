aa5655928cf7a4113ca9f77201029a20c8219b2f
eunbeechoi <noninvertingamp@gmail.com>
2023-09-14 17:26:51 +0900

    #8 finished

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -32,19 +32,28 @@ export const watch = async(req, res) => {
 
 export const getEdit = async(req, res) => {
     const { id } = req.params;
+    const {user: {_id}} = req.session;
     const video = await Video.findById(id);
 
     if(!video) {
         return res.status(404).render("404", {pageTitle: "Video not found."});
     }
+    console.log(video.owner, _id);
+    if(String(video.owner) !== String(_id)){
+        return res.status(404).redirect("/")
+    }
     return res.render("edit", {pageTitle: `Edit ${video.title}`, video});
 };
 export const postEdit = async(req, res) => {
+    const {user: {_id}} = req.session;
     const { id } = req.params;
     const { title, description, hashtags} = req.body;
     const video = await Video.exists({_id: id});
     if(!video) {
         return res.status(404).render("404", {pageTitle: "Video not found."});
+    }
+    if(String(video.owner) !== String(_id)){
+        return res.status(404).redirect("/")
     }
     await Video.findByIdAndUpdate(id, { 
         title,
@@ -66,7 +75,7 @@ export const postUpload = async (req, res) => {
     const { title, description, hashtags } = req.body;
     console.log("eevee", path);
     try {
-        await Video.create({
+        const newVideo = await Video.create({
             title,
             description,
             fileUrl: path,
@@ -75,6 +84,9 @@ export const postUpload = async (req, res) => {
             createdAt: Date.now(),
             hashtags: Video.formatHashtags(hashtags),
            });
+        const user = await User.findById(_id);
+        user.videos.push(newVideo._id);
+        user.save(); 
         return res.redirect("/");
     } catch(error){
         return res.status(400).render("upload", {
@@ -87,8 +99,17 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async(req, res) => {
     const { id } = req.params;
+    const {user: {_id}} = req.session;
+    const video = await Video.findById(id);
+    if(!video){
+        return res.status(404).render("404", {pageTitle: "Video not found."});
+    }
+    if(String(video.owner) !== String(_id)){
+        return res.status(404).redirect("/")
+    }
     await Video.findByIdAndDelete(id);
         //de;ete video
+    
     return res.redirect("/")
 
 };
--- src/models/User.js
+++ src/models/User.js
@@ -9,12 +9,17 @@ const userSchema = new mongoose.Schema({
     password: {type: String},
     name: {type: String, required: true},
     location: String,
+    videos: [{type: mongoose.Schema.Types.ObjectId, ref: "Video"}]  //Video 모델에 연결 
 
 });
 
 //비밀번호 저장
 userSchema.pre('save', async function(){   //save하기 전에 async 함수 사용 
-    this.password = await bcrypt.hash(this.password, 5);    //this는 유저컨트롤러 - postJoin에서 create되는 User를 가르킴 / 이 함수가 비번 hash
+    //property가 하나라도 수정되면 isModified true
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 5);    //this는 유저컨트롤러 - postJoin에서 create되는 User를 가르킴 / 이 함수가 비번 hash
+    }
+    
 });
 
 const User = mongoose.model("User", userSchema);
