41a90078915976f263fcb5b0a437d179efcfc62a
eunbeechoi <noninvertingamp@gmail.com>
2023-09-14 15:39:25 +0900

    #8.12

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -17,14 +17,16 @@ export const home = async(req, res) => {
     const videos = await Video.find({}).sort({ createdAt: "desc"});
     return res.render("home", { pageTitle: "Home", videos});
 };
+
+
 export const watch = async(req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id);
-    const owner = await User.findById(video.owner);
+    const video = await Video.findById(id).populate("owner");
+    console.log(video)
     if(!video){
         return res.render("404", {pageTitle: "Video not found."});
     }
-    return res.render("watch", {pageTitle: video.title, video, owner});
+    return res.render("watch", {pageTitle: video.title, video});
 };
 
 
