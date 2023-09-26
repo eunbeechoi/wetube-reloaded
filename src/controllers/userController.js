4a9e59913cb1036420b24002b285e0ce92bf9ef5
eunbeechoi <noninvertingamp@gmail.com>
2023-09-26 14:45:00 +0900

    #10.3 finished

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -211,12 +211,29 @@ export const postChangePassword = async(req, res) => {
     return res.redirect("/users/logout");
 };
 
-export const see = async(req, res) => {
+/*export const see = async(req, res) => {
     const {id} = req.params;
     const user = await User.findById(id).populate("videos");
     if(!user){
         return res.status(404).render("404", { pageTitle : "User not found."});
     }
     return res.render("users/profile", {pageTitle: `${user.name}`, user});
+}; */
+
+
+export const see = async(req, res) => {
+    const {id} = req.params;
+    const user = await User.findById(id).populate({
+        path: "videos",
+        populate: {
+            path: "owner",
+            model: "User",
+        },
+    });
+    if(!user){
+        return res.status(404).render("404", { pageTitle : "User not found."});
+    }
+    return res.render("users/profile", {pageTitle: `${user.name}`, user});
 };
+
 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -14,7 +14,7 @@ Video.find({}, (error, videos) => {
 */
 
 export const home = async(req, res) => {
-    const videos = await Video.find({}).sort({ createdAt: "desc"});
+    const videos = await Video.find({}).sort({ createdAt: "desc"}).populate("owner");
     return res.render("home", { pageTitle: "Home", videos});
 };
 
@@ -122,7 +122,7 @@ export const search = async(req, res) => {
             title: {
                 $regex: new RegExp(keyword, "i")   //i는 대소문자 구분X, keyword를 포함하는 영상 찾아줌
             }          
-        });
+        }).populate("owner");
     }
     return res.render("search", { pageTitle: "Search", videos});
 };
